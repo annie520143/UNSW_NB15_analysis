b4124b830f2ff52659b7ae3f9abf1e1354d4f5bf
annie520143 <annie520143@gmail.com>
2020-10-27 15:24:30 +0800

    change main_dnn.py

--- method_dl/main_dnn.py
+++ method_dl/main_dnn.py
@@ -22,125 +22,59 @@ from keras.models import Sequential
 import method_dnn as method 
 #import temp_iptable as iptable
 
-def init(packets, result_opt):
-    #deal with missing
-    packets.fillna(value=0, inplace=True)  # fill missing with 0
-
-    if(result_opt == 'attack_cat'):
-        packets, attack_cat = prep.seperate_att_lab_catagory(packets)
-        #packets, attack_cat = prep.seperate_att_lab_kmeans(packets)
-        
-        #if we want to do get specfic
-        packets = prep.get_imp(packets)
-
-        try: packets = prep.proto_to_value(packets)
-        except: pass  
-        try: packets = prep.state_to_value(packets)
-        except: pass
-        try: packets = prep.service_to_value(packets)
-        except: pass
-
-        
-        #packets, srcip = prep.ip_to_value(packets)
-        #print(packets.keys())
-
-        return packets, attack_cat
-    elif(result_opt == 'label'):
-        packets, label = prep.seperate_att_lab_label(packets)
-        #if we want to do get specfic
-        packets = prep.get_imp(packets)
-
-        packets = prep.proto_to_value(packets)    
-        packets = prep.state_to_value(packets)   
-        packets = prep.service_to_value(packets)
-        #packets, srcip = prep.ip_to_value(packets)
-        return packets, label
-
-    
 #create np array for label
-def label_to_nparr(label_list):
-    """ 
-    label_np = []
-    onehoten = OneHotEncoder(sparse=False)
-    label_np = onehoten.fit_transform(label_list.reshape(-1, 1)).astype(int)
-    #print(label_np.shape) 
-    """
-
-    #如果原本資料沒有那一類 one hot出來就沒有！！ 
-  
-    label_np = []
-    for i in range(label_list.shape[0]):
-        if(label_list[i] == 0):
-            label_np.append([1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        elif(label_list[i] == 1):
-            label_np.append([0,1,0,0,0,0,0,0,0,0])
-        elif(label_list[i] == 2):
-            label_np.append([0,0,1,0,0,0,0,0,0,0])
-        elif(label_list[i] == 3):
-            label_np.append([0,0,0,1,0,0,0,0,0,0])
-        elif(label_list[i] == 4):
-            label_np.append([0,0,0,0,1,0,0,0,0,0])
-        elif(label_list[i] == 5):
-            label_np.append([0,0,0,0,0,1,0,0,0,0])
-        elif(label_list[i] == 6):
-            label_np.append([0,0,0,0,0,0,1,0,0,0])
-        elif(label_list[i] == 7):
-            label_np.append([0,0,0,0,0,0,0,1,0,0])
-        elif(label_list[i] == 8):
-            label_np.append([0,0,0,0,0,0,0,0,1,0])
-        elif(label_list[i] == 9):
-            label_np.append([0,0,0,0,0,0,0,0,0,1])
-
-    label_np = np.array(label_np) 
+def categoryOneHot(label, opt): 
     
+    cat_n = 2 if(opt == 'label') else 10
+    label_np = []
+
+    for i in range(label.shape[0]):      
+        for index in range(cat_n):
+            if(label[i] == index):
+                a = [0]*cat_n
+                a[index] = 1
+                label_np.append(a)
+
+    label_np = np.array(label_np)
     return label_np
 
 
 
-def processed_data(datapath, result_opt):
-    data_df = pd.read_csv(datapath, low_memory=False)
+
+def processed_data(datapath, opt):
     
-    if(result_opt == 'attack_cat'):
-        data_df, attcat_list = init(data_df, result_opt)
-        data_df = prep.del_useless_features(data_df)
-        #transforming datatype
-        data_df_transtype = prep.trans_datatype(data_df)
+    packets = pd.read_csv(datapath, low_memory=False)
+    
 
-        #scaling (data type changes after scaling, i.e. df -> np)
-        data_df_scale = prep.feature_scaling(data_df_transtype)
+    packets.fillna(value=0, inplace=True)
 
-        #create an one-hot list for label list
-        attcat_list_oneHot = label_to_nparr(attcat_list)
+    packets, attack_cat = prep.seperate_att_lab_catagory(packets)
 
-        #turn dataframe and list to np array
-        attcat_np, data_np = np.array(attcat_list_oneHot), np.array(data_df_scale)
-        #deal with problem of key 'ct_ftp_cmd'
+    packets = prep.get_imp(packets)
+
+    try: packets = prep.proto_to_value(packets)
+    except: pass  
+    try: packets = prep.state_to_value(packets)
+    except: pass
+    try: packets = prep.service_to_value(packets)
+    except: pass
+
+    #transforming datatype
+    packets = prep.trans_datatype(packets)
+
+    #scaling (data type changes after scaling, i.e. df -> np)
+    packetScaled = prep.feature_scaling(packets)
+
+    #create an one-hot list for label list
+    attcatOneHot = categoryOneHot(attack_cat,opt)
+
+    #turn dataframe and list to np array
+    attcat_np, packets_np = np.array(attcatOneHot), np.array(packetScaled)
         
-        data_np = prep.np_fillna(data_np)
+    #deal with problem of key 'ct_ftp_cmd'
+    packets_np = prep.np_fillna(packets_np)
 
-
-        return data_np, attcat_np, attcat_list
-
-    elif(result_opt == 'label'):
-        data_df, label_list = init(data_df, result_opt)
-        data_df = prep.del_useless_features(data_df)
-        
-
-        #transforming datatype
-        data_df_transtype = prep.trans_datatype(data_df)
-
-        #scaling (data type changes after scaling, i.e. df -> np)
-        data_df_scale = prep.feature_scaling(data_df_transtype)
-
-        #create an one-hot list for label list
-        datalabel_list_oneHot = label_to_nparr(label_list)
-        
-        #turn dataframe and list to np array
-        label_np, data_np = np.array(datalabel_list_oneHot), np.array(data_df_scale)
-        #deal with problem of key 'ct_ftp_cmd'
-        data_np = prep.np_fillna(data_np)
-
-        return data_np, label_np, label_list
+    return packets_np, attcat_np, attack_cat
 
 def info():
     print('Basic info:')
