58cbab8372e5b42cfed50b3a9a934cfc7595c1e6
annie520143 <annie520143@gmail.com>
2020-10-27 16:43:06 +0800

    main_dnn.py ver2

--- method_dl/main_dnn.py
+++ method_dl/main_dnn.py
@@ -2,33 +2,28 @@ import numpy as np
 import pandas as pd
 import sys
 
-"""
-callback function
-"""
+#keras
 from keras.callbacks import CSVLogger
 from keras.callbacks import ModelCheckpoint
 from keras.callbacks import EarlyStopping
+from keras.models import Sequential
 import keras.models as ks
 from sklearn.preprocessing import OneHotEncoder
 
-"""
-preprocessing
-"""
+#preprocessing
 import preprocessing as prep
-"""
-Keras Method
-"""
-from keras.models import Sequential
+
+#DNN Method
 import method_dnn as method 
-#import temp_iptable as iptable
+
 
 #create np array for label
-def categoryOneHot(label, opt): 
+def CategoryOneHot(label, opt): 
     
     cat_n = 2 if(opt == 'label') else 10
     label_np = []
 
-    for i in range(label.shape[0]):      
+    for i in range(len(label)):
         for index in range(cat_n):
             if(label[i] == index):
                 a = [0]*cat_n
@@ -39,64 +34,58 @@ def categoryOneHot(label, opt):
     return label_np
 
 
-
-
-def processed_data(datapath, opt):
+def ProcessData(datapath, opt):
     
     packets = pd.read_csv(datapath, low_memory=False)
     
-
     packets.fillna(value=0, inplace=True)
 
-    packets, attack_cat = prep.seperate_att_lab_catagory(packets)
+    packets, attack_cat, label = prep.SeperateAttackLabel(packets)
 
-    packets = prep.get_imp(packets)
+    packets = prep.GetImp(packets)
+    packets = prep.FeatureOneHot(packets)
 
-    try: packets = prep.proto_to_value(packets)
-    except: pass  
-    try: packets = prep.state_to_value(packets)
-    except: pass
-    try: packets = prep.service_to_value(packets)
-    except: pass
 
     #transforming datatype
-    packets = prep.trans_datatype(packets)
+    packets = prep.TransDatatype(packets)
 
     #scaling (data type changes after scaling, i.e. df -> np)
-    packetScaled = prep.feature_scaling(packets)
+    packetScaled = prep.FeatureScaling(packets)
 
     #create an one-hot list for label list
-    attcatOneHot = categoryOneHot(attack_cat,opt)
+    attcatOneHot = CategoryOneHot(attack_cat,opt)
 
     #turn dataframe and list to np array
     attcat_np, packets_np = np.array(attcatOneHot), np.array(packetScaled)
         
     #deal with problem of key 'ct_ftp_cmd'
-    packets_np = prep.np_fillna(packets_np)
+    packets_np = prep.NpFillna(packets_np)
+
+    #print(packets_np)
 
     return packets_np, attcat_np, attack_cat
 
 def info():
     print('Basic info:')
-    print('training dataset:', train_path)
-    print('testing dataset: ', test_path)
-    print('model: ', used_model)
+    print('training dataset:', trainPath)
+    print('testing dataset: ', testPath)
+    print('model: ', usedModel)
     print('===================================')
 
 
-train_path = "../dataset/test_balance.csv"
-test_path = "../dataset/1_10-18_mix_time.csv"
+trainPath = "../dataset/1_1-2_label0_time.csv"
+testPath = "../dataset/1_10-18_mix_time.csv"
 
-expected_output = 'attack_cat'
-used_model = 'model/dnn_selfdef1_random.h5'
+opt = 'attack_cat'
+usedModel = 'model/dnn_selfdef1_random.h5'
 
 
 if __name__ == "__main__":
 
 
     #label depends on expected_output
-    train_np, trainlabel_np, trainlabel_list = processed_data(train_path, expected_output)
-    test_np, testlabel_np, testlabel_list = processed_data(test_path, expected_output)
+    train_np, trainlabel_np, trainlabel_list = ProcessData(trainPath, opt)
+    test_np, testlabel_np, testlabel_list = ProcessData(testPath, opt)
 
     pd.set_option('display.max_columns', None)
     pd.set_option('display.max_rows', None)
@@ -105,19 +94,17 @@ if __name__ == "__main__":
     feature_dim = train_np.shape[1] # how mant features
     output_dim = trainlabel_np.shape[1]
 
-    print(output_dim)
-    
 
     # simpleDNN_dropout(feature_dim, units, atv, loss)
-    if(expected_output == 'label'):
+    if(opt == 'label'):
         model = method.simpleDNN_dropout(feature_dim, 32, 'relu', 'mse', output_dim)
-    elif(expected_output == 'attack_cat'):
+    elif(opt == 'attack_cat'):
         model = method.simpleDNN_dropout(feature_dim, 32, 'relu', 'mse', output_dim)
 
     # Setting callback functions
     csv_logger = CSVLogger('training.log')
 
-    checkpoint = ModelCheckpoint(filepath=used_model,
+    checkpoint = ModelCheckpoint(filepath=usedModel,
                                 verbose=1,
                                 save_best_only=True,
                                 monitor='accuracy',
@@ -129,24 +116,26 @@ if __name__ == "__main__":
     
     #training
     model.fit(train_np, trainlabel_np, batch_size=100, epochs=100, callbacks=[earlystopping, checkpoint, csv_logger], shuffle=True)
-    #model.fit(train_np, trainlabel_np, batch_size=100, epochs=10, shuffle=True)
-
+   
+    #traing result
     result = model.evaluate(train_np,  trainlabel_np)
     print("training accuracy = ", result[1])
-   
 
-    model = ks.load_model(used_model)
+    model = ks.load_model(usedModel)
     #print(model.summary())
     
+    #testing result
     result = model.evaluate(test_np,  testlabel_np)
     print("testing accuracy = ", result[1])
     
+    #predict result
     predictLabel = model.predict_classes(test_np)
-    #shape(1 * dataset_size)
-    np.set_printoptions(threshold=sys.maxsize)
-
-    method.matricsDNN(predictLabel, testlabel_list, expected_output, output_dim)
-    method.detailAccuracyDNN(predictLabel, testlabel_list, expected_output, output_dim)
+    
+    method.matricsDNN(predictLabel, testlabel_list, opt, output_dim)
+    method.detailAccuracyDNN(predictLabel, testlabel_list, opt, output_dim)
     #method.comparePredict(test_path, predictLabel, testlabel_list, expected_output )
+
+    #np.set_printoptions(threshold=sys.maxsize)
+
 
 
--- method_dl/preprocessing.py
+++ method_dl/preprocessing.py
@@ -2,26 +2,22 @@ from sklearn.preprocessing import MinMaxScaler
 #from sklearn.preprocessing import Imputer
 import numpy as np
 import pandas as pd
+import re
 
 # all feature, except srcip dstip
 all_features = ['sport', 'dsport', 'proto', 'state', 'dur', 'sbytes', 'dbytes', 'sttl', 'dttl', 'sloss', 'dloss', 'service', 'Sload', 'Dload', 'Spkts', 'Dpkts', 'swin', 'dwin', 'stcpb', 'dtcpb', 'smeansz', 'dmeansz', 'trans_depth', 'res_bdy_len', 'Sjit', 'Djit', 'Stime', 'Ltime','Sintpkt', 'Dintpkt', 'tcprtt', 'synack', 'ackdat', 'is_sm_ips_ports', 'ct_state_ttl', 'ct_flw_http_mthd', 'is_ftp_login', 'ct_ftp_cmd', 'ct_srv_src', 'ct_srv_dst', 'ct_dst_ltm', 'ct_dst_ltm', 'ct_src_dport_ltm', 'ct_dst_sport_ltm', 'ct_dst_src_ltm', 'attackCat', 'Label']
 
-# 
 imp_features = ['sport', 'dsport', 'proto', 'state', 'dur', 'sbytes', 'dbytes', 'sttl', 'dttl', 'sloss', 'dloss', 'service', 'Sload', 'Dload', 'Spkts', 'Dpkts', 'swin', 'dwin', 'stcpb', 'dtcpb', 'smeansz', 'dmeansz', 'trans_depth', 'res_bdy_len', 'Sjit', 'Djit', 'Stime', 'Ltime',
                 'Sintpkt', 'Dintpkt', 'tcprtt', 'synack', 'ackdat', 'is_sm_ips_ports', 'ct_state_ttl', 'ct_flw_http_mthd', 'is_ftp_login', 'ct_ftp_cmd', 'ct_srv_src', 'ct_srv_dst', 'ct_dst_ltm', 'ct_dst_ltm', 'ct_src_dport_ltm', 'ct_dst_sport_ltm', 'ct_dst_src_ltm', 'attackCat', 'Label']
 
-paper_features = ['proto','state','dbytes', 'sttl', 'dttl', 'sloss', 'dloss',  'service','Sload', 'Dload', 'Spkts', 'Dpkts', 'swin', 'dwin','stcpb', 'smeansz', 'Djit', 'synack' 'is_sm_ips_ports', 'ct_state_ttl', 'is_ftp_login', 'ct_ftp_cmd', 'ct_srv_src', 'ct_srv_dst', 'ct_dst_ltm', 'ct_dst_ltm', 'ct_src_dport_ltm', 'ct_dst_sport_ltm', 'ct_dst_src_ltm', 'attackCat', 'Label']
-
-#
-oneHotFeatures = ['proto', 'states', 'service']
-
 #mutable
-proto = ['tcp', 'udp', 'arp', 'ospf', 'ip', 'icmp', 'unas']
-states = ['FIN', 'CON', 'REQ', 'URH', 'ACC', 'CLO',  'ECO', 'ECR','INT', 'MAS', 'PAR',  'RST', 'TST', 'TXD',  'URN', 'RSTO', ]
-service = ['dns', 'smtp', 'http', 'ftp', 'ftp-data', 'pop3', 'ssh', 'dhcp', 'ssl', 'snmp', 'radius', 'irc']
+oneHotDict = {
+'proto' : ['tcp', 'udp', 'arp', 'ospf', 'ip', 'icmp', 'unas'],
+'states' : ['FIN', 'CON', 'REQ', 'URH', 'ACC', 'CLO',  'ECO', 'ECR','INT', 'MAS', 'PAR',  'RST', 'TST', 'TXD',  'URN', 'RSTO'],
+'service' : ['dns', 'smtp', 'http', 'ftp', 'ftp-data', 'pop3', 'ssh', 'dhcp', 'ssl', 'snmp', 'radius', 'irc']
+}
 
-
-attackCat = ['[\s]+', '[\s]?F(.)*', '[\s]?A(.)*', '[\s]?B(.)*', '[\s]?D(.)*', '[\s]?E(.)*', '[\s]?G(.)*', '[\s]?R(.)*', '[\s]?S(.)*', '[\s]?W(.)*']
+attackCat = ['[\s]?A(.)*', '[\s]?B(.)*', '[\s]?D(.)*', '[\s]?E(.)*', '[\s]?G(.)*', '[\s]?R(.)*', '[\s]?S(.)*', '[\s]?W(.)*']
 
 
 def SeperateAttackLabel(packets):
@@ -29,109 +25,52 @@ def SeperateAttackLabel(packets):
     newAttackCat = []
     newLabel = []
 
-    
-    for value in enumerate(dataAttackCat):
-
+    re_match = 0
+    for value in dataAttackCat:
+        flag = 1
         for i, cat in enumerate(attackCat):
-            if (value == cat):
-                newAttack.append(i)
+            try:
+                re_match = re.match(cat, value)
+            except: pass
+
+            if ((((re_match!= None) | (value == 0)) & (flag))):
+                flag = 0
+                if value == 0:
+                    newAttackCat.append(0)
+                else:
+                    newAttackCat.append(i+1)
 
                 if(i != 0):
                     newLabel.append(1)
                 else:
-                    newLable.append(0)
+                    newLabel.append(0)
 
     del packets['attack_cat']
     del packets['Label']
-    
 
     return packets, newAttackCat, newLabel
-
 
 
 #one hot encoding
 def FeatureOneHot(packets):
 
-    for feature in oneHotFeatures:
+    for feature in oneHotDict.keys():
         try:
             data = packets[feature]
-            for element in feature:
-
-            tempList = []
-            for d in data:
-                if (d == element):
-                    tempList.append(1)
-                else:
-                    tempList.append(0)
-            packets[element] = tempList
+            for element in oneHotDict[feature]:
+                tempList = []
+                for d in data:
+                    if (d == element):
+                        tempList.append(1)
+                    else:
+                        tempList.append(0)
+                packets[element] = tempList
 
         except:
             pass
 
-        
-
     return packets
         
-"""
-def proto_to_value(packets):
-
-    for element in proto:   
-
-        proto_list = []
-        for protocol in packets['proto']:
-
-            if(protocol == element):
-                proto_list.append(1)
-            else:
-                proto_list.append(0)
-
-        packets[element] = proto_list
-
-    del packets['proto']
-
-
-    return packets
-
-
-
-def state_to_value(packets):
-
-    for element in states:
-        state_list = []
-        for state in packets['state']:
-
-            if(state == element):
-                state_list.append(1)
-            else:
-                state_list.append(0)
-
-        packets[element] = state_list
-
-    del packets['state']
-
-    return packets
-    
-
-
-def service_to_value(packets):
-
-
-    for element in service:
-        service_list = []
-        for serv in packets['service']:
-
-            if(serv == element):
-                service_list.append(1)
-            else:
-                service_list.append(0)
-
-        packets[element] = service_list
-
-    del packets['service']
-
-    return packets
-    
-"""
 
 def GetImp(packets):
     imp_features_n = len(imp_features) 
@@ -151,7 +90,6 @@ def GetImp(packets):
     return packets_imp
 
 
-
 #normalization
 def FeatureScaling(packets):
     sc = MinMaxScaler(feature_range=(0, 1))
@@ -159,7 +97,6 @@ def FeatureScaling(packets):
     packets_scaled = sc.fit_transform(packets)
 
     return packets_scaled
-
 
 
 def TransDatatype(packets):
